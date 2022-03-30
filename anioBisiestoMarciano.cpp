1fce25a17b529a1c0fda178ce4d64f0c2a469076
Fernando Mauro <lopezmauro1973f@gmail.com>
2022-03-29 22:05:05 -0600

    anio bisiesto no terminado

---
+++ anioBisiestoMarciano.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   int dia,mes, anio;
+   cin >> dia >> mes >> anio;
+   //todos los meses tienen 57 dias excepto febrero que tendra 59, si anio % 5 == 0 entonces febrero tendra 58 dias :).
+   //comproba si es multiplo de 5 :,)
+   if(anio % 5 == 0){
+      
+   }
+   return 0;
+}
