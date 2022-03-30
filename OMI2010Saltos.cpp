cf2e04168c1696268320a18012d967a5cc0bd127
Fernando Mauro <lopezmauro1973f@gmail.com>
2022-03-29 20:11:16 -0600

    Terminado problema de saltos

--- OMI2010Saltos.cpp
+++ OMI2010Saltos.cpp
@@ -1,32 +1,29 @@
+//https://omegaup.com/arena/problem/L--OMI2010-Saltos/#problems
 #include <iostream>
 
 using namespace std;
 
-int mcd(int a , int b){
-   while(a != 0 && b != 0){
-      if(a > b){
-         a = a % b;
-      }else{
-         b = b % a;
-      }
-   }
-   if( a == 0){
-      return b;
+long long int mcd(long long int mayor, long long int menor){
+   if(mayor % menor == 0){
+      return menor;
    }else{
-      return a;
+      long long int resto = mayor % menor;
+      return mcd(menor, resto);
    }
 }
-int mcm(int a, int b){
-   int auxiliar = 0;
-   if( a > b){
-      auxiliar = a / mcd(a,b);
-      return b * auxiliar;
-   }else{
-      auxiliar = b / mcd(a,b);
-      return b * auxiliar;
-   }
+long long int mcm(long long int a, long long int b){
+   if(a > b){
+      return (a * b) / mcd(a,b);
+    }else{
+      return (a * b) / mcd(b,a);
+    }
 }
 int main(){
-   cout << mcm(3,5) / 5;
+   long long int m,n,a,d,f,c;
+   cin >> m >> n >> a >> d >> f >> c;
+   long long int mcmHorizontal = mcm(n,d) / d;
+   long long int mcmVertical = mcm(m,a) / a;
+   long long int resultado = mcm(mcmHorizontal, mcmVertical);
+   cout << resultado;
    return 0;
 }
