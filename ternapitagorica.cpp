d6a0d15329b21a4739e25131d16254e2089cdff1
Fernando Mauro <lopezmauro1973f@gmail.com>
2022-05-12 19:47:30 -0500

    Ternan pitagorica modificada

--- ternapitagorica.cpp
+++ ternapitagorica.cpp
@@ -1,23 +1,7 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main(){
-   int primero, segundo, tercero;
-   for(int c = 1; c < 1000; c++){
-      for(int b = 1; b < 1000; b++){
-         for(int a = 1; a < 1000; a++){
-            if(a + b + c == 1000){
-               if((a * a) + (b * b) == (c * c)){
-                  primero = a;
-                  segundo = b;
-                  tercero = c;
-                  break;
-               }
-            }
-         }
-      }
-   }
-   cout << primero * segundo * tercero;
-   return 0;
+   vector <vector <int>> numeros (10, vector <int> (10));
 }
