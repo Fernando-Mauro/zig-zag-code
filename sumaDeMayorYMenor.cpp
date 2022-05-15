712f4e1ad8a1f6de6b305a222883ad13d30c3131
Fernando Mauro <lopezmauro1973f@gmail.com>
2022-05-15 10:42:26 -0500

    Suma de mayor y menos

---
+++ sumaDeMayorYMenor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+   cin.tie(nullptr);
+   ios_base::sync_with_stdio(false);
+   int n;
+   cin >> n;
+   vector <int> numeros(n);
+   for(int i = 0; i < n; i++){
+      cin >> numeros[i];
+   }
+   sort(numeros.begin(), numeros.end());
+   cout << numeros[0] + numeros[numeros.size() - 1];
+   return 0;
+}
