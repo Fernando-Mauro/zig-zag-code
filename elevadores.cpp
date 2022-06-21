68752c3866122bbb4befe65deab9415bb9fabbb0
Fernando Mauro <lopezmauro1973f@gmail.com>
2022-06-20 22:40:22 -0500

    Casi terminado elevadores pero tiene un bug en cuanto a la posicion de roco D:?

---
+++ elevadores.cpp
@@ -0,0 +1,59 @@
+// https://omegaup.com/arena/problem/Elevadores
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct elevador
+{
+   int posicion;
+   int pisoInicial;
+   int pisoFinal = pisoInicial + 1;
+};
+
+int caminar(int posicionRoco, int pisoActualRoco, vector<elevador> &elv, int extremo)
+{
+   // pisoInicial del elevador que esta en su misma posicion (horizontal)
+   while (posicionRoco != extremo)
+   {
+      int pisoInicialElv = elv[posicionRoco - 1].pisoInicial;
+      int pisoFInalElv = elv[posicionRoco - 1].pisoFinal;
+      posicionRoco++;
+      if (posicionRoco == extremo)
+      {
+         return pisoActualRoco;
+      }
+      if (pisoInicialElv == pisoActualRoco)
+      {
+         // cout << "Bingo, hay un elevador que sube frente a mi";
+         pisoActualRoco++;
+      }
+      else if (pisoFInalElv == pisoActualRoco)
+      {
+         // cout << "Bingo, hay un elevador que baja frente a mi";
+         pisoActualRoco--;
+      }
+   }
+   return pisoActualRoco;
+}
+int main()
+{
+   int pisos, elevadores;
+   cin >> pisos >> elevadores;
+   vector<elevador> elv(elevadores);
+   for (int i = 0; i < elevadores; i++)
+   {
+      int inicial;
+      cin >> inicial;
+      elv[i].posicion = i + 1;
+      elv[i].pisoInicial = inicial;
+      elv[i].pisoFinal = inicial + 1;
+   }
+   for (int i = 0; i < pisos; i++)
+   {
+      // posicion horizontal
+      int posicionRoco = 1;
+      // piso vertical
+      int piso = i + 1;
+      cout << caminar(posicionRoco, piso, elv, elevadores) << endl;
+   }
+}
