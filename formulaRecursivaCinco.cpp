3a64db75e076aef8eb446e4c804e66ab96dad57f
Fernando Mauro <lopezmauro1973f@gmail.com>
2022-06-25 18:25:15 -0500

    formulaRecursivaCinco ahora si con 100 puntos

--- formulaRecursivaCinco.cpp
+++ formulaRecursivaCinco.cpp
@@ -4,14 +4,11 @@ using namespace std;
 
 int funcion(int n, int m){
    if(n == 1){
-      return -1 * funcion(n - 2, m);
+      return (-1 * funcion(n - 2, m));
+   }else if(n > 3){
+      return (2 + funcion(n - 1, m));
    }
-   if(n == 2){
-      return 2 * m;
-   }
-   if(n >= 3){
-      return 2 + funcion(n - 1, m);
-   }
+   return 2 * m;
 }
 
 int main(){
