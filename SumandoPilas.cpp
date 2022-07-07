72ec6622d19c9bff9dcebeb1e1c789b7d33ba6d9
Fernando Mauro <lopezmauro1973f@gmail.com>
2022-07-07 10:48:43 -0500

    SUmando pilas terminado

---
+++ SumandoPilas.cpp
@@ -0,0 +1,37 @@
+// https://omegaup.com/arena/problem/Sumando-con-pilas
+#include <bits/stdc++.h>
+
+using namespace std;
+stack <int> pila;
+void agrega(int n){
+   pila.push(n);
+}
+void consume(){
+   int suma = 0;
+   suma += pila.top();
+   pila.pop();
+   suma += pila.top();
+   pila.pop();
+   pila.push(suma);
+}
+void imprimir (){
+   cout << pila.top() << endl;
+}
+int main(){
+   int instrucciones = 0;
+   cin >> instrucciones;
+   string cadena;
+   int aux = 0;
+   while(instrucciones--){
+      cin >> cadena;
+      if(cadena == "AGREGA"){
+         cin >> aux;
+         agrega(aux);
+      }else if( cadena == "CONSUME"){
+         consume();
+      }else{
+         imprimir();
+      }
+   }
+   return 0;
+}
