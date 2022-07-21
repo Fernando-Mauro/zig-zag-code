47ab31e3919b64e554817e2b94c4607b47ffefad
OZZY <a.santiagocsh@gmail.com>
2022-07-20 20:05:15 -0500

    teayudo

--- BFS/escapeDelLaberinto.cpp
+++ BFS/escapeDelLaberinto.cpp
@@ -1,63 +1,68 @@
 // https://omegaup.com/arena/problem/Escape-del-laberinto/
 #include <bits/stdc++.h>
-
 using namespace std;
 
-typedef pair <int, int> coordenadas;
+typedef vector<vector<char> > Matriz;
 
-vector<int> changeFilas = { -1, 0, 1, 0 };
+vector<int> fils = { -1, 0, 1, 0 };
+vector<int> cols = { 0, 1, 0, -1 };
 
-vector<int> changeColumnas = { 0, 1, 0, -1 };
+bool busqueda(Matriz &laberinto, int i, int j, Matriz &solucion) {
+  
+  char posActual = laberinto[i][j];
 
-bool bandera = false;
-// y -> first
-// x -> second 
-bool DFS(vector <vector<char>> &laberinto, coordenadas actual){
-   // explorar hacia arriba, abajo, derecha, izquierda
-   if( laberinto[actual.first][actual.second] == 'S'){
-      bandera = true;
-      return true;
-   } 
-   coordenadas nuevasCoordenadas;
-   for(int i = 0; i < 4; i++){
-      // comprobar que estemos dentro de los limites aun
-      nuevasCoordenadas.first = actual.first + changeFilas[i];
-      nuevasCoordenadas.second = actual.second +  changeColumnas[i];
-      if(nuevasCoordenadas.first < laberinto.size() && nuevasCoordenadas.second <laberinto[0].size() && nuevasCoordenadas.first >= 0 && nuevasCoordenadas.second >= 0){
-         if(laberinto[nuevasCoordenadas.first][nuevasCoordenadas.second] != '#'){
-            if(laberinto[nuevasCoordenadas.first][nuevasCoordenadas.second] == 'S'){
-               bandera = true;
-               break;
-            }
-            if(laberinto[nuevasCoordenadas.first][nuevasCoordenadas.second] == ' '){
-               laberinto[nuevasCoordenadas.first][nuevasCoordenadas.second] = '#';
-               DFS(laberinto, nuevasCoordenadas);
-            }
-         }
+  if (posActual == 'S') {
+    return true;
+  }
+
+  for (int k = 0; k < 4; k++) {
+    int nx = i + fils[k];
+    int ny = j + cols[k];
+    bool noSaleMatriz = (nx >= 0 && nx < laberinto.size() && ny >= 0 && ny < laberinto[nx].size());
+
+    if (noSaleMatriz && laberinto[nx][ny] != '#' && solucion[nx][ny] == ' ') {
+      solucion[nx][ny] = '#';
+      if (busqueda(laberinto, nx, ny, solucion)) {
+        return true;
       }
-   }
+    }
+  }
+
+  return false;
 }
-int main(){
-   cin.tie(nullptr);
-   ios_base::sync_with_stdio(false);
-   int filas, columnas;
-   cin >> filas >> columnas;
-   vector <vector<char>> laberinto(filas, vector <char> (columnas));
-   cin.ignore();
-   string aux;
-   coordenadas entrada;
-   for(int i = 0; i < filas; ++i){
-      getline(cin, aux);
-      for(int j = 0; j < aux.size(); j++){
-         laberinto[i][j] = aux[j];
-         if(aux[j] == 'E') entrada = {i,j};
+
+int main() {
+  int filas, columnas;
+  cin>>filas>>columnas;
+  cin.ignore();
+
+  Matriz laberinto = Matriz(filas, vector<char>(columnas, ' '));
+  Matriz solucion = Matriz(filas, vector<char>(columnas, ' '));
+  
+  for (int i=0; i<filas; i++) {
+    string s;
+    getline(cin,s);
+    for (int j=0; j<s.size(); j++) {
+      laberinto[i][j] = s[j];
+    }
+  }
+
+  bool tieneSalida = false;
+  for (int i=0; i<filas; i++) {
+    for (int j=0; j<columnas; j++) {
+      if (laberinto[i][j] == 'E') {
+        solucion[i][j] = '#';
+        tieneSalida = busqueda(laberinto, i, j, solucion);
+        break;
       }
-   }
-   // cout << entrada.first << entrada.second << endl;
-   // cout << salida.first << salida.second << endl;
-   coordenadas actual = entrada;
-   DFS(laberinto, actual);
-   if(bandera) cout << "SI" << endl;
-   else cout << "NO" << endl;
-   return 0;
+    }
+  }
+
+  if (tieneSalida) {
+    cout<<"Si"<<endl;
+  } else {
+    cout<<"No"<<endl;
+  }
+
+  return 0;
 }
