24d7622f460f615a924f9851940832565a47beab
OZZY <a.santiagocsh@gmail.com>
2022-08-07 14:44:38 -0500

    Nuevo Problema de Matrices

---
+++ Nidos-de-tortuga.cpp
@@ -0,0 +1,15 @@
+//https://omegaup.com/arena/problem/Nidos-de-tortuga/
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector<vector<int>> Matriz;
+
+int main() {
+    int filas, columnas;
+
+    cin >> filas >> columnas;
+
+    Matriz playa()
+
+    return 0;
+}
