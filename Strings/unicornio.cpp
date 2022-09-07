b9b2418d274d6b64a1d55ac6016cb3d109ae7a0d
Fernando Mauro <fernando@debian.fernando>
2022-09-06 21:47:40 -0500

    Problema del unicornio parcialmente terminado

---
+++ Strings/unicornio.cpp
@@ -0,0 +1,41 @@
+// https://omegaup.com/arena/problem/Unicornio/
+#include <bits/stdc++.h>
+
+using namespace std;
+/*
+                    /
+               ,.. /
+             ,'   ';
+  ,,.__    _,' /';  .
+ :','  ~~~~    '. '~
+:' (   )         )::,
+'. '. .=----=..-~  .;'
+ '  ;'  ::   ':.  '"
+   (:   ':    ;)
+    \\   '"  ./
+     '"      '"
+Un unicornio para entrar en contexto :)
+*/
+string unicornio = "unicornio";
+
+void comprobar(string &cadena){
+   if(cadena.size() < 9) {
+      cout << -0;
+      return;
+   }
+   for(int i = 0; i < unicornio.size(); ++i){
+      if(unicornio[i] != tolower(cadena[i])){
+         cout << cadena[i]; 
+         return;
+      }
+   }
+   cout << 0;
+}
+int main(){
+   cin.tie(nullptr);
+   ios_base::sync_with_stdio(false);
+   string cadena;
+   cin >> cadena;
+   comprobar(cadena);
+   return 0;
+}
