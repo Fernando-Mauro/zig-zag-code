1ede46138866d768c6d097593aa40adfe431b228
Fernando Mauro <lopezmauro1973f@gmail.com>
2022-10-30 20:24:03 -0600

    Watermelon solve

---
+++ CodeForces/watermelon.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/problemset/problem/4/A
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+   cin.tie(nullptr);
+   ios_base::sync_with_stdio(false);
+   int num;
+   cin >> num;
+   if(num % 2 == 0 && num > 2){
+      cout << "YES" << endl;
+      return 0;
+   }else{
+      cout << "NO" << endl;
+   }
+   return 0;
+}
