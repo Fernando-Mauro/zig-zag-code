a57b230e25e160c1855f20c7e29308282539eb69
Fernando Mauro <lopezmauro1973f@gmail.com>
2023-01-01 13:50:07 -0600

    Ejercicio en java resuelto

---
+++ CPBook/Capitulo 1/Ejercicio1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Ejercicio1{
+   public static void main(String [] args){
+      Scanner sc = new Scanner(System.in);
+      Double number = sc.nextDouble();
+      System.out.printf("%7.3f\n", number);
+   }
+}
