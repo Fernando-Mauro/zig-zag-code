aa371f23334bcf820ddef05f4f4a92e0d0a48478
Fernando Mauro <lopezmauro1973f@gmail.com>
2023-01-01 13:54:46 -0600

    Ejercicio 2 resuelto en c++

---
+++ CPBook/Capitulo 1/Ejercicio2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+   cin.tie(nullptr);
+   ios_base::sync_with_stdio(false);
+   int n;
+   cin >> n;
+   cout << setprecision(n + 1) << M_PI;
+   return 0;
+}
