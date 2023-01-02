94dde5ac0c4c8daf179548a28bd4a728404d9434
Fernando Mauro <lopezmauro1973f@gmail.com>
2023-01-01 20:08:24 -0600

    Solucion terminada

---
+++ Nivel Basico/bienvenidosAC++2.cpp
@@ -0,0 +1,13 @@
+// https://omegaup.com/arena/problem/B01-Bienvenido-a-C-II/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+   cin.tie(nullptr);
+   ios_base::sync_with_stdio(false);
+   int n;
+   cin >> n;
+   while(n--) cout << "Bienvenido a C++" << endl;
+   return 0;
+}
