8e0e0eda3a84fd13be259efbeabf773027afb2da
Fernando Mauro <lopezmauro1973f@gmail.com>
2023-01-01 20:06:15 -0600

    Solucion a 'Bienvenidos a c++ 2 ' en C terminada

---
+++ Nivel Basico/bienvenidosAC++2.c
@@ -0,0 +1,9 @@
+// https://omegaup.com/arena/problem/B01-Bienvenido-a-C-II/
+#include <stdio.h>
+
+int main(){
+   int n;
+   scanf("%d", &n);
+   while(n--) printf("Bienvenido a C++\n");
+   return 0;
+}
