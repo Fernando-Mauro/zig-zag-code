f11e9e8b1da4b3b59a36e87f7fcdaa656c015b5c
Fernando Mauro <lopezmauro1973f@gmail.com>
2023-01-02 22:00:06 -0600

    Problema resuelto por completo

--- CPBook/Capitulo 1/Ejercicio3.py
+++ CPBook/Capitulo 1/Ejercicio3.py
@@ -1,4 +1,5 @@
 from datetime import date
 anterior = date(2010, 8, 9)
 actual = date.today()
-print(anterior)
+print(anterior.strftime("%A")) #Usamos %a para el dia de la semana abreviado y %A para el dia de la semana completo
+print("{}".format((actual - anterior).days))
