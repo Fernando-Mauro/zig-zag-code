b3707426b73233a15c139c2dc44eda5dbc7024cb
Fernando Mauro <lopezmauro1973f@gmail.com>
2023-01-24 17:35:15 -0600

    Moskow Dream problema terminado de manera satisfactorio

---
+++ Nivel Basico/moscowDream.cpp
@@ -0,0 +1,14 @@
+// https://open.kattis.com/problems/moscowdream
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+   int a,b,c,n; scanf("%d%d%d%d", &a, &b, &c, &n);
+   if(a >= 1 && b >= 1 && c >= 1 && ((a + b + c) >= n) && n >= 3){
+      printf("YES\n");
+   }else{
+      printf("NO\n");  
+   }
+   return 0;
+}
