b652ee28d0ffbdd2d3d78a6fe0dbc5a4744137e1
Fernando Mauro <fermaurolf@gmail.com>
2023-07-19 13:24:04 -0600

    Back to the past in c++ solved

---
+++ UvA/backToThePast.cpp
@@ -0,0 +1,11 @@
+// https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=866&page=show_problem&problem=4913
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    printf("May 29, 2013 Wednesday\n");
+    return 0;
+}
