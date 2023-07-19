4a7183e76b337bf497281a4a9bf062b26eab95b9
Fernando Mauro <fermaurolf@gmail.com>
2023-07-19 14:26:44 -0600

    The last problem solved

---
+++ kattis/thelastproblem.cpp
@@ -0,0 +1,13 @@
+// https://open.kattis.com/problems/thelastproblem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    char s [100];
+    scanf("%[^\n]%*c", s);
+    printf("Thank you, %s ,and farewell!\n", s);
+    return 0;
+}
