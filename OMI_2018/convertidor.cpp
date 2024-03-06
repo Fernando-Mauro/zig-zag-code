5757e1c0f5475123bb247c740a19ecb3b48dd5d1
Fernando Mauro <fermaurolf@gmail.com>
2024-03-05 21:36:51 -0600

    Convertidor solucionado

--- OMI_2018/convertidor.cpp
+++ OMI_2018/convertidor.cpp
@@ -2,18 +2,34 @@
 
 using namespace std;
 
+typedef unsigned long long int i64;
+
 char decimalString(){
+    i64 decimal = 0;
+    cin >> decimal;
+    string cadena = "";
+    while(decimal != 0){
+        decimal -= 1;
+        int res = decimal % 26;
+        decimal /= 26;
+        cadena.push_back(char(res + 65));
+    }
+    for(int i = (cadena.size() - 1); i >=0; --i){
+        cout << cadena[i];
+    }
+    return 0;
 }
 
-int stringDecimal(){
+i64 stringDecimal(){
     string varchar;
     cin.ignore();
     getline(cin, varchar);
-    int suma = 0;
-    for(int i = 0; i < varchar.size(); ++i){
+    i64 suma = 0;
+    int power = 0;
+    for(int i = (varchar.size() - 1); i >= 0; --i){
         int value = int(varchar[i]) - 64;
-        int pos = (varchar.size() - i);
-        suma += value * pow(26, pos);
+        suma += (value * pow(26, power));
+        power++;
     }
     return suma;
 }
@@ -22,9 +38,8 @@ int main(){
     bool type;
     cin >> type;
     switch(type){
-        case false: cout << stringDecimal(); break;
-        case true: decimalString(); break;
+        case true: cout << stringDecimal(); break;
+        case false: decimalString(); break;
     }
-
     return 0;
 }
